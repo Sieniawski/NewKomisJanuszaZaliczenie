d9f202a6491bfdfbff9501c57e4575b4163882fa
sieniawski7 <sieniawski7@wp.pl>
2018-12-08 18:02:29 +0100

    add class Pojazd add class Samochod add class Motocykl

---
+++ NewKomisJanuszaZaliczenie/Pojazd.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewKomisJanuszaZaliczenie
+{
+    class Pojazd
+    {
+        private float f_cenaZakupu;
+        private float f_marza;
+        private int i_rokProdukcji;
+        
+        //getery podst wartosci
+        public float Marza
+        {
+            get
+            {
+                return f_marza;
+            }
+        }
+
+        public float CenaZakupu
+        {
+            get {
+                return f_cenaZakupu;
+            }
+        }
+
+        public int RokProdukcji
+        {
+            get {
+                return i_rokProdukcji;
+            }
+        }
+
+        void wyswietlRok()
+        {
+
+        }
+
+        float podajCeneKoncowa()
+        {
+            float cenaKoncowa = CenaZakupu + Marza;
+            return cenaKoncowa;
+        }
+
+        //public abstract string wyswietlMarke();
+        //public abstract string wyswietlModel();
+        //public abstract string wyswietlTypPojazdu();
+    }
+}
--- NewKomisJanuszaZaliczenie/Program.cs
+++ NewKomisJanuszaZaliczenie/Program.cs
@@ -10,6 +10,17 @@ namespace NewKomisJanuszaZaliczenie
     {
         static void Main(string[] args)
         {
+            /*MENU
+                1. Wyswietl listę pojazdów
+                2. Dodaj nowy pojazd
+                    Funkcja Dodaj nowy pojazd wyswietli menu dostepnych
+                    pojazdów do dodania. Wymaga wprowadzenia wartości zakupu,marzy i rocznika
+                3. Wyszukaj pojazdy
+                    1. Wyszukaj po przedziale cenowym : od do
+                    2. Wyszukaj po przedziale rodznikowym : od do
+                4. Sprzedaj pojazd
+            */
+            Console.ReadKey();
         }
     }
 }
